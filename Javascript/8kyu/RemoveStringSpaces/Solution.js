8736b727cc2eb5e7fd2229df9b9b783b1a624a67
Mateusz Lis <lismateusz2005@gmail.com>
2023-06-18 23:43:24 +0200

    Solve Remove string spaces

---
+++ Javascript/8kyu/RemoveStringSpaces/Solution.js
@@ -0,0 +1,5 @@
+function noSpace(x){
+    return x.split(' ').join('') // make x an array by using split(' ') (it creates array separated by argument () in this case (' '), then make it to str without spaces by join('')
+}
+
+console.log(noSpace('8 j 8   mBliB8g  imjB8B8  jl  B'));

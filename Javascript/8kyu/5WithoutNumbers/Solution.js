1f1825ca78e438312c79f5fbd83434b286357cc3
MateuszTate <lismateusz2005@gmail.com>
2023-06-20 20:31:22 +0200

    Solve 5 without numbers

---
+++ Javascript/8kyu/5WithoutNumbers/Solution.js
@@ -0,0 +1,6 @@
+function unusualFive() {
+    let array = ['m','a','t','e','u'];
+    return array.length;
+
+    // make an array with 5 elements and return lenght of this arr, by using build in method .lenght just to avoid using number 
+  }

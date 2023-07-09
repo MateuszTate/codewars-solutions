38d2f04960649e036971cf5e544628c50d291a84
MateuszTate <lismateusz2005@gmail.com>
2023-07-09 20:36:46 +0200

    Solve Row Weigths

---
+++ Javascript/7kyu/RowWeights/Solution.js
@@ -0,0 +1,11 @@
+function rowWeights(array){
+    let sum1 = 0;
+    let sum2 = 0; // variables for both sums equal to 0 at the start
+    for(let i = 0; i<array.length; i++){
+        if(i%2==0) sum1+=array[i]; // if current index is even number add it to the sum1
+        else if(i%2!=0) sum2+=array[i]; // if current index is odd number add it to the sum2
+    }
+    return [sum1, sum2]; // return sum1, sum2 as an array;
+  }
+
+console.log(rowWeights([100,50,5,2]));

345f581594e42d42a5201d281eb08bcd80caaac8
Mateusz Lis <lismateusz2005@gmail.com>
2023-07-21 23:43:12 +0200

    Solve What is between

---
+++ Javascript/8kyu/WhatIsBetween/Solution.js
@@ -0,0 +1,8 @@
+function between(a, b) {
+    let result = []; // empty array for result
+    for (a; a <= b; a++) {
+        // start from a until its equal to b, add one for every number
+        result.push(a); // push current number to the array
+    }
+    return result; // return array as function output
+}

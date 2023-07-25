8e6111ff3cd6d8351d776dc2fd03ebcfb81fa8f7
MateuszTate <lismateusz2005@gmail.com>
2023-07-25 12:27:26 +0200

    Kata example twist

---
+++ Javascript/8kyu/KataExampleTwist/Solution.js
@@ -0,0 +1,5 @@
+// add the value "codewars" to the websites array 1,000 times
+var websites = []
+for(let i = 0; i<1000; i++){
+    websites.push('codewars'); // for loop going 1000 times and for every times push codewars string to the websites
+}

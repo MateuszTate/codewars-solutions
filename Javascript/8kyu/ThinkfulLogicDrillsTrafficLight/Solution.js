62fe3a23362fc6876b30ebb49f33b842cad31ff3
MateuszTate <lismateusz2005@gmail.com>
2023-09-13 19:46:03 +0200

    Solve thinkful logic drills traffic light

---
+++ Javascript/8kyu/ThinkfulLogicDrillsTrafficLight/Solution.js
@@ -0,0 +1,13 @@
+function updateLight(current) {
+    switch (current) {
+        case 'green':
+            return 'yellow';
+        case 'yellow':
+            return 'red';
+        case 'red':
+            return 'green';
+    }
+  
+  }
+
+console.log(updateLight('green'));
